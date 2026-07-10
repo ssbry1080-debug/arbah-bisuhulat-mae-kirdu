971b0fb4dd638f369f83285c747b880bbee96505
ssbry1080-debug <ssbry1080@gmail.com>
2026-07-10 14:39:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+function togglePassword() {
+    const password = document.getElementById("password");
+    const eye = document.querySelector(".eye");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAgLbyqaXLyev-dyt3K2MysbxMsQcPxG1c",
-  authDomain: "arbah-bisuhulat-mae-kirdu.firebaseapp.com",
-  projectId: "arbah-bisuhulat-mae-kirdu",
-  storageBucket: "arbah-bisuhulat-mae-kirdu.firebasestorage.app",
-  messagingSenderId: "283220939343",
-  appId: "1:283220939343:web:cdd52e9964316faa20503a"
-};
+    if (password.type === "password") {
+        password.type = "text";
+        eye.classList.remove("fa-eye");
+        eye.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        eye.classList.remove("fa-eye-slash");
+        eye.classList.add("fa-eye");
+    }
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-
-document.getElementById("signup").addEventListener("click", () => {
-  createUserWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => {
-      alert("تم إنشاء الحساب بنجاح");
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-});
-
-document.getElementById("login").addEventListener("click", () => {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => {
-      alert("تم تسجيل الدخول بنجاح");
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+document.querySelector(".login").addEventListener("click", function () {
+    alert("مرحبًا بك في منصة اربح بسهولة مع كردو عفرين");
 });
