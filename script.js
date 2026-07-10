bf94438bb06cf5f3b4894959318c396bf9afe30e
ssbry1080-debug <ssbry1080@gmail.com>
2026-07-10 14:49:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function togglePassword() {
-    const password = document.getElementById("password");
-    const eye = document.querySelector(".eye");
 
-    if (password.type === "password") {
-        password.type = "text";
-        eye.classList.remove("fa-eye");
-        eye.classList.add("fa-eye-slash");
-    } else {
-        password.type = "password";
-        eye.classList.remove("fa-eye-slash");
-        eye.classList.add("fa-eye");
-    }
-}
-
-document.querySelector(".login").addEventListener("click", function () {
-    alert("مرحبًا بك في منصة اربح بسهولة مع كردو عفرين");
-});
